98ad7ddea4fe0978bf65202e9306836527934af2
Jan Janda <janjanda42@gmail.com>
2021-05-26 14:51:07 +0200

    Set up alerts evaluation

--- server/services/alerts-eval.js
+++ server/services/alerts-eval.js
@@ -1,23 +1,63 @@
 'use strict';
 
-const log = require('../lib/log');
 const fetch = require('node-fetch');
 const signalSets = require('../models/signal-sets');
 const adminContext = require('../lib/context-helpers').getAdminContext();
 
-async function insert1() { //pridat try-catch a rekurzivne pres setTimeout
-    const fetched = await fetch('https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json');
-    const result = await fetched.json();
+async function insertCovid() {
+    try {
+        const fetched = await fetch('https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.json');
+        const result = await fetched.json();
+        const data = result.data[0];
 
-    const sigSetWithSigMap = await signalSets.getById(adminContext, 2, false, true);
-    await signalSets.insertRecords(adminContext, sigSetWithSigMap, [{id: '2', signals: {siga1: 314}}]);
+        const sigs = {
+            deaths: data.umrti,
+            newCases: data.potvrzene_pripady_vcerejsi_den,
+            newTests: data.provedene_testy_vcerejsi_den,
+            newCases65: data.potvrzene_pripady_65_vcerejsi_den,
+            inHospital: data.aktualne_hospitalizovani
+        };
+        const record = {id: data.datum, signals: sigs};
 
+        const sigSetWithSigMap = await signalSets.getById(adminContext, 1, false, true);
+        await signalSets.insertRecords(adminContext, sigSetWithSigMap, [record]);
+    }
+    catch (e) {}
 
-    log.info('Alerts', 'Alerts evaluator started');
+    const minuteDelay = Math.floor((Math.random() * 180) + 180);
+    setTimeout(insertCovid, minuteDelay * 60 * 1000);
+}
+
+async function insertWeather() {
+    try {
+        const fetchedPrague = await fetch('http://api.openweathermap.org/data/2.5/find?units=metric&q=Prague,cz&appid=3f9ed61a70b43a7d7f2913f40ff2d4eb');
+        const fetchedVimperk = await fetch('http://api.openweathermap.org/data/2.5/find?units=metric&q=Vimperk,cz&appid=3f9ed61a70b43a7d7f2913f40ff2d4eb');
+        const resultPrague = await fetchedPrague.json();
+        const resultVimperk = await fetchedVimperk.json();
+
+        const sigs = {
+            weatherP: resultPrague.list[0].weather[0].main,
+            weatherV: resultVimperk.list[0].weather[0].main,
+            tempP: resultPrague.list[0].main.temp,
+            tempV: resultVimperk.list[0].main.temp,
+            humP: resultPrague.list[0].main.humidity,
+            windSpeedP: resultPrague.list[0].wind.speed,
+            windDegP: resultPrague.list[0].wind.deg,
+        };
+        const record = {id: resultPrague.list[0].dt.toString(), signals: sigs};
+
+        const sigSetWithSigMap = await signalSets.getById(adminContext, 2, false, true);
+        await signalSets.insertRecords(adminContext, sigSetWithSigMap, [record]);
+    }
+    catch (e) {}
+
+    const minuteDelay = Math.floor((Math.random() * 10) + 10);
+    setTimeout(insertWeather, minuteDelay * 60 * 1000);
 }
 
 function run() {
-    setTimeout(insert1, 5000);
+    //setTimeout(insertCovid, 60 * 1000);
+    //setTimeout(insertWeather, 70 * 1000);
 }
 
 module.exports.run = run;
